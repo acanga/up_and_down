07b381c4b72529efe5804d4ffa5f940442a306da
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-10-28 11:08:26 -0200

    Translating Devise routes to portuguese.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 UpAndDown::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :path => "usuarios", :path_names => { :sign_up => "cadastrar", :sign_in => "entrar", :sign_out => "sair", :edit_user => "alterar", :password => "senha" } 
   
   # root :to => 'welcome#index'
 end
