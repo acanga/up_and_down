0dba3a4e228e09618a8afafa6793a97fbf5f8251
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-10-28 10:49:06 -0200

    First commit! Just adding the basic Rails application produced by rails new command.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UpAndDown::Application.config.secret_token = '9b0b029c277524e78b234882cfb1154cce04c45a7ff4e487cbfc190e26914598431bf3d62e6f3e21a9e2080374a8d748a823c1384344d4d131a7a9edbeb235d8'
