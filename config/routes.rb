32e2be7d01b0e1942b2b6c640d513662c8184d59
Tati Carvalho <tatigmc@yahoo.com.br>
2011-10-28 15:34:05 -0200

    Renamed the route for stockholder. Were using the old customer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 UpAndDown::Application.routes.draw do                                                                                                                                          
   devise_for :users, :path => "usuarios", :path_names => { :sign_up => "cadastrar", :sign_in => "entrar", :sign_out => "sair", :edit_user => "alterar", :password => "senha" }   
   
-  resources :customer_activity_reports
+  resources :stockholder_base_reports
 
   # root :to => 'welcome#index'
 end
