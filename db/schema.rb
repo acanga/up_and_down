c8db2bec518403e2165163f40046d776068d36a0
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-10-28 15:34:40 -0200

    Updating schema.rb.

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(:version => 20111028163450) do
 
-  create_table "stockholder_base_report", :force => true do |t|
+  create_table "stockholder_base_reports", :force => true do |t|
     t.string   "excel_report"
     t.datetime "created_at"
     t.datetime "updated_at"
