082fc1da8ee00c992cf8b0ccc339d7c208016bcc
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-10-28 17:13:42 -0200

    Fixing validates_format_of validation for excel report.

--- app/models/stockholder_base_report.rb
+++ app/models/stockholder_base_report.rb
@@ -2,5 +2,5 @@ class StockholderBaseReport < ActiveRecord::Base
   mount_uploader :excel_report, ExcelReportUploader
 
   validates_presence_of :excel_report
-  validates_format_of :excel_report, :with => /(\.xls|\.xlsx)$/i, :allow_nil => true
+  validates_format_of :excel_report, :with => /(\.xls|\.xlsx)$/i, :allow_blank => true
 end                                                
