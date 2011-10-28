671f8f58ea484a0dcbc3a0a9fad0565fe104c44b
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-10-28 16:08:25 -0200

    Improving routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 UpAndDown::Application.routes.draw do                                                                                                                                          
   devise_for :users, :path => "usuarios", :path_names => { :sign_up => "cadastrar", :sign_in => "entrar", :sign_out => "sair", :edit_user => "alterar", :password => "senha" }   
   
-  resources :stockholder_base_reports
+  resources :stockholder_base_reports, :path => "relatorio_movimentacao_acionistas", :path_names =>  { :new => "cadastrar" }
 
-  # root :to => 'welcome#index'
+  root :to => "stockholder_base_reports#index"
 end
