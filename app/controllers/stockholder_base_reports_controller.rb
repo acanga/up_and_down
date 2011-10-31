6c64b5fbec3940915f80e998a747d24e63c72148
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-10-31 17:36:34 -0200

    Adding image to submit.

--- app/controllers/stockholder_base_reports_controller.rb
+++ app/controllers/stockholder_base_reports_controller.rb
@@ -1,5 +1,12 @@
 class StockholderBaseReportsController < InheritedResources::Base
   before_filter :authenticate_user!
+
+  def index
+    @stockholder_base_report = StockholderBaseReport.new
+    @stockholder_base_reports = StockholderBaseReport.scoped
+
+    respond_with @stockholder_base_reports
+  end
   
   protected
     def begin_of_association_chain
