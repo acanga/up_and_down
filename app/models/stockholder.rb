7b6a6461b2023e7be0023c54840c5a5951b19f46
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-10-31 14:40:11 -0200

    Adding basic parse of excel report.

--- app/models/stockholder.rb
+++ app/models/stockholder.rb
@@ -1,3 +1,6 @@
 class Stockholder < ActiveRecord::Base
   belongs_to :stockholder_base_report
+
+  # validates_presence_of :name, :kind, :amount_of_stocks_pn, :pn_percentage, :amount_of_stocks_on,
+    # :on_percentage, :total_stocks
 end
--- app/models/stockholder_base_report.rb
+++ app/models/stockholder_base_report.rb
@@ -6,4 +6,26 @@ class StockholderBaseReport < ActiveRecord::Base
   
   validates_presence_of :excel_report
   validates_format_of :excel_report, :with => /(\.xls|\.xlsx)$/i, :allow_blank => true
-end                                                
+
+  after_save :parse_excel_report
+
+  protected
+    def parse_excel_report
+      excel_report_file = excel_report_object
+      excel_report_file.default_sheet = excel_report_file.sheets.first
+
+      6.upto(excel_report_file.last.row) do |line|
+        Stockholder.create(:name => excel_report_file.cell(line, 1))
+      end
+    end
+
+    def excel_report_object
+      if excel_report.to_s.match /xlsx$/i
+        excel_class = Excelx 
+      else
+        excel_class = Excel
+      end
+      
+      excel_class.new File.join(Rails.root, "public", excel_report.to_s)
+    end
+end
