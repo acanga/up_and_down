dbf79de1cda1eb37587afc6df3bc57da23763696
Tati Carvalho <tatigmc@yahoo.com.br>
2011-10-31 15:49:50 -0200

    Inclusao do enqueue e extracao do metodo parse_excel_report para o worker

--- app/models/stockholder_base_report.rb
+++ app/models/stockholder_base_report.rb
@@ -11,21 +11,6 @@ class StockholderBaseReport < ActiveRecord::Base
 
   protected
     def parse_excel_report
-      excel_report_file = excel_report_object
-      excel_report_file.default_sheet = excel_report_file.sheets.first
-
-      6.upto(excel_report_file.last.row) do |line|
-        Stockholder.create(:name => excel_report_file.cell(line, 1))
-      end
-    end
-
-    def excel_report_object
-      if excel_report.to_s.match /xlsx$/i
-        excel_class = Excelx 
-      else
-        excel_class = Excel
-      end
-      
-      excel_class.new File.join(Rails.root, "public", excel_report.to_s)
+      Resque.enqueue(StockholderBaseReportParser, self.id)
     end
 end
