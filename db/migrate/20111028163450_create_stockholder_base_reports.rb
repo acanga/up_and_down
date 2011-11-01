babad2ca9ecedcae66d8878924ea194b9be1db1d
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-11-01 17:25:05 -0200

    Updating migrations.

--- db/migrate/20111028163450_create_stockholder_base_reports.rb
+++ db/migrate/20111028163450_create_stockholder_base_reports.rb
@@ -2,7 +2,7 @@ class CreateStockholderBaseReports < ActiveRecord::Migration
   def change
     create_table :stockholder_base_reports do |t|
       t.references :user
-      t.boolean :unparsed, :default => false
+      t.boolean :parsed, :default => false
       t.string :excel_report  
 
       t.timestamps
