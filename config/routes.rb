f5daae2fe641051ebb3887efcf05efff0189689b
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-11-01 09:55:00 -0200

    Updating routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 UpAndDown::Application.routes.draw do                                                                                                                                          
-  devise_for :users, :path => "usuarios", :path_names => { :sign_up => "cadastrar", :sign_in => "entrar", :sign_out => "sair", :edit_user => "alterar", :password => "senha" }   
+  devise_for :users, :path => "usuarios", :path_names => { :sign_up => "cadastrar", :sign_in => "entrar", 
+    :sign_out => "sair", :edit_user => "alterar", :password => "senha" }   
   
-  resources :stockholder_base_reports, :path => "relatorio_movimentacao_acionistas", :path_names =>  { :new => "cadastrar" }
+  resources :stockholder_base_reports, :path => "relatorio_de_movimentacao_dos_acionistas", :path_names =>  { :new => "cadastrar" }
+  post "/relatorio_de_movimentacao_dos_acionistas/cadastrar" => "stockholder_base_reports#create", :as => create_stockholder_base_report
 
   root :to => "stockholder_base_reports#index"
 end
