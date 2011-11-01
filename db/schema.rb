f9839c73b82096c0ab503fd1032de3ca7afdb406
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-11-01 15:46:17 -0200

    Adding a flag to identify if a report is processed or not.

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(:version => 20111031123310) do
 
   create_table "stockholder_base_reports", :force => true do |t|
     t.integer  "user_id"
+    t.boolean  "processed",    :default => false
     t.string   "excel_report"
     t.datetime "created_at"
     t.datetime "updated_at"
