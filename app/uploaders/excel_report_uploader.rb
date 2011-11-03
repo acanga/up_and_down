a9dbbd5a80c297cf4f3a681e40b69db62e4b4c42
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-11-03 10:46:44 -0200

    Cleaning excel report uploader.

--- app/uploaders/excel_report_uploader.rb
+++ app/uploaders/excel_report_uploader.rb
@@ -1,5 +1,3 @@
-# encoding: utf-8
-
 class ExcelReportUploader < CarrierWave::Uploader::Base
   include CarrierWave::MimeTypes
   
@@ -10,13 +8,7 @@ class ExcelReportUploader < CarrierWave::Uploader::Base
   end
 
   process :set_content_type
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
+  
   # def extension_white_list
   #   %w(xls xlsx)
   # end
