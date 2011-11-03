5745af3049f56a41c622b3825f3b9afbbe3c17fb
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-11-03 15:33:58 -0200

    Changing the URL where sign up form submits to.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 UpAndDown::Application.routes.draw do                                                                                                                                          
   devise_for :users, :path => "usuarios", :path_names => { :sign_up => "cadastrar", :sign_in => "entrar", 
-    :sign_out => "sair", :edit_user => "alterar", :password => "senha" }   
+    :sign_out => "sair", :edit_user => "alterar", :password => "senha" } do
+      post "/usuarios/cadastrar" => "devise/registrations#create", :as => :create_user
+  end
   
   resources :stockholder_base_reports, :actions => [ :new, :create, :index ], :path => "relatorio_de_movimentacao_dos_acionistas", 
     :path_names =>  { :new => "cadastrar" }
