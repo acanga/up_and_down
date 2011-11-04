4db77f29d7196e7e40584daa9f99f2cba629c5ce
Tati Carvalho <tatigmc@yahoo.com.br>
2011-11-04 11:12:51 -0200

    Adicionando helper para ajustar tamanho box login/cadastro conforme apresentar erro/warning.Atualizando view correspondente e css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
   include HeadHelpers
+  
+  def id_for_main_content_block
+    if resource.errors.any? or alert.present?
+      "login_largura_maior"
+    else
+      "login"
+    end
+  end
 end
