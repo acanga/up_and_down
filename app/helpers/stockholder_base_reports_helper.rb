3f9cbd410430437eafeb33725e4599718fd04a74
Tati Carvalho <tatigmc@yahoo.com.br>
2011-11-09 11:29:11 -0200

    Ajustei relatorio para mostrar campos de acordo com o excel.

--- app/helpers/stockholder_base_reports_helper.rb
+++ app/helpers/stockholder_base_reports_helper.rb
@@ -1,11 +1,11 @@
 module StockholderBaseReportsHelper
   def diff_columns_for(stockholder)
-    diff_between(:current_version => stockholder.amount_of_stocks_pn, :previous_version => stockholder.previous_version.try(:amount_of_stocks_pn)) +
-      diff_between(:current_version => stockholder.pn_percentage, :previous_version => stockholder.previous_version.try(:pn_percentage)) +
-      diff_between(:current_version => stockholder.amount_of_stocks_on, :previous_version => stockholder.previous_version.try(:amount_of_stocks_on)) +
-      diff_between(:current_version => stockholder.on_percentage, :previous_version => stockholder.previous_version.try(:on_percentage)) +
-      diff_between(:current_version => stockholder.total_stocks, :previous_version => stockholder.previous_version.try(:total_stocks)) +
-      diff_between(:current_version => stockholder.total_percentage, :previous_version => stockholder.previous_version.try(:total_percentage))
+    diff_between(:current_version => stockholder.amount_of_stocks_on, :previous_version => stockholder.previous_version.try(:amount_of_stocks_on)) +
+     diff_between(:current_version => stockholder.on_percentage, :previous_version => stockholder.previous_version.try(:on_percentage)) +
+     diff_between(:current_version => stockholder.amount_of_stocks_pn, :previous_version => stockholder.previous_version.try(:amount_of_stocks_pn)) +
+     diff_between(:current_version => stockholder.pn_percentage, :previous_version => stockholder.previous_version.try(:pn_percentage)) +
+     diff_between(:current_version => stockholder.total_stocks, :previous_version => stockholder.previous_version.try(:total_stocks)) +
+     diff_between(:current_version => stockholder.total_percentage, :previous_version => stockholder.previous_version.try(:total_percentage))
   end
 
   protected
