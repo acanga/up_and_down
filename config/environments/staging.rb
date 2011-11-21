2385b7d41020538b3a7c3dbc1497393cda0d75fd
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-11-21 14:41:58 -0200

    Improving staging config.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,17 +1,17 @@
 UpAndDown::Application.configure do
-  config.cache_classes = true
+  config.cache_classes = false
   config.action_controller.perform_caching = true
+  
+  config.whiny_nils = true
+  config.active_support.deprecation = :log
 
-  config.consider_all_requests_local = false
-  config.active_support.deprecation = :notify
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }  
+  config.action_mailer.perform_deliveries = true
+  
+  config.consider_all_requests_local = true
+  config.action_dispatch.best_standards_support = :builtin
 
-  config.serve_static_assets = false
-  config.assets.compress = true
-  config.assets.compile = false
-  config.assets.digest = true
-  config.assets.precompile += %w(admin/admin.css admin/admin.js)
-
-  config.i18n.fallbacks = true
-
-  config.action_mailer.default_url_options = { :host => "up-and-down.com" }
+  config.assets.compress = false
+  config.assets.debug = true
 end
